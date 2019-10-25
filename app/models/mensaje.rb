a6e5e10073f5d29fe5dfd234b9c79474b13d231a
David Rico <dfricoh@unal.edu.co>
2019-10-25 12:05:42 -0500

    Update mensaje.rb

--- app/models/mensaje.rb
+++ app/models/mensaje.rb
@@ -1,5 +1,5 @@
 class Mensaje < ApplicationRecord
     def self.conversacion(id_cliente, id_establecimiento)
-        where(id_cliente: id_cliente, id_establecimiento: id_establecimiento).order(created_at: :desc)
+        where(id_cliente: id_cliente, id_establecimiento: id_establecimiento).order(created_at: :asc)
     end
 end
